70cea4f08b2a678f5b1892878714c5aa2878f104
Emil Ågren <ea225mc@student.lnu.se>
2026-09-24 20:57:44 +0200

    Add 3sum_cache

--- Part1 - Evaluate Three Sum Algorithms/three_sum_problem.py
+++ Part1 - Evaluate Three Sum Algorithms/three_sum_problem.py
@@ -12,3 +12,28 @@ def threesum_brute(lst, sum=0):
           result.add(tuple(sorted((lst[index1], lst[index2], lst[index3]))))
   return list(result)
 
+def threesum_cache(lst, sum=0):
+  result = set()
+
+  for index1 in range(len(lst)):
+    cache = set()
+    # print("\nList:", lst)
+
+    for index2 in range(index1 + 1, len(lst)):
+      value1 = lst[index1]
+      value2 = lst[index2]
+      value3 = sum - value1 - value2
+      # print("Checking:", value1, value2, "need:", value3, "cache:", cache)
+
+      if value3 in cache:
+        result.add(tuple(sorted((value1, value2, value3))))
+      cache.add(value2)
+    # print("Result:", list(result))
+
+  return list(result)
+
+
+lst = generate_random_list(15, 15)
+print("List:", lst)
+print("Brute:", sorted(threesum_brute(lst)))
+print("Cache:", sorted(threesum_cache(lst)))
