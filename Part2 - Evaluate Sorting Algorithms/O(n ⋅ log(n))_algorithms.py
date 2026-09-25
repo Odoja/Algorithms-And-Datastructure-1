0692e58222ed50deb58f717a96cccee8b10cf10d
Emil Ågren <ea225mc@student.lnu.se>
2026-09-25 13:51:55 +0200

    Add quick sort

--- Part2 - Evaluate Sorting Algorithms/O(n ⋅ log(n))_algorithms.py
+++ Part2 - Evaluate Sorting Algorithms/O(n ⋅ log(n))_algorithms.py
@@ -16,7 +16,6 @@ def merge_sort(list_):
 
   return merge(sorted_left, sorted_right)
 
-
 def merge(left_value, right_value):
   list_ = []
   left_index, right_index = 0, 0
@@ -43,5 +42,33 @@ def merge(left_value, right_value):
 
   return list_
 
-def quick_sort():
+def quick_sort(list_):
+  length = len(list_)
   
+  if length <= 1:
+    return list_
+  
+  first_number, left_list, right_list = partition(list_)
+
+  # Recursively calls quick_sort until the list is split into 2 containing single elements
+  # Those smaller than the first number to the left and those larger to the right.
+  sorted_left = quick_sort(left_list)
+  sorted_right = quick_sort(right_list)
+
+  return sorted_left + [first_number] + sorted_right
+
+def partition(list_):
+  length = len(list_)
+
+  first_number = list_[0]
+  left_list, right_list = [], []
+
+  for index in range(1, length):
+    secound_number = list_[index]
+    # Compare the secound number with the first number and place it in the left or right list.
+    if secound_number <= first_number:
+      left_list += [secound_number]
+    else:
+      right_list += [secound_number]
+  return first_number, left_list, right_list
+
