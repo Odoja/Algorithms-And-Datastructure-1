019440d003f7d538666f267aa7d22e3bceab9b12
Emil Ågren <ea225mc@student.lnu.se>
2026-09-25 04:03:03 +0200

    Add bubble sort

--- Part2 - Evaluate Sorting Algorithms/O(n^2)_algorithms.py
+++ Part2 - Evaluate Sorting Algorithms/O(n^2)_algorithms.py
@@ -1,3 +1,16 @@
-def merge_sort():
+def selection_sort(list_):
 
-def quick_sort():
+def bubble_sort(list_):
+  length = len(list_)
+
+  for iteration_count in range(length):
+    swapped = False
+    for current_index in range(length - iteration_count - 1):
+      if list_[current_index] > list_[current_index + 1]:
+        # If the current element is greater than the next element, swap places
+        list_[current_index], list_[current_index + 1] = list_[current_index + 1], list_[current_index]
+        swapped = True
+    if not swapped:
+      break
+
+def insertion_sort(list_):
