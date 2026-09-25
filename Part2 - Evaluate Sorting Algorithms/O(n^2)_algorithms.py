29d03f8725b384e78e3ecee3077a8ee08085aefd
Emil Ågren <ea225mc@student.lnu.se>
2026-09-25 04:28:45 +0200

    Add selection sort and comments

--- Part2 - Evaluate Sorting Algorithms/O(n^2)_algorithms.py
+++ Part2 - Evaluate Sorting Algorithms/O(n^2)_algorithms.py
@@ -1,10 +1,24 @@
 def selection_sort(list_):
+  length = len(list_)
+
+  for current_index in range(length - 1):
+    index_of_smallest_value = current_index
+
+    # Search the rest of the list for a smaller value.
+    for compare_index in range(current_index + 1, length):
+      if list_[compare_index] < list_[index_of_smallest_value]:
+        index_of_smallest_value = compare_index
+    # Swap the smallest value into the current index position
+    list_[current_index], list_[index_of_smallest_value] = list_[index_of_smallest_value], list_[current_index]
+
 
 def bubble_sort(list_):
   length = len(list_)
 
   for iteration_count in range(length):
     swapped = False
+
+    # Compare neighboring values in the unsorted part of the list.
     for current_index in range(length - iteration_count - 1):
       if list_[current_index] > list_[current_index + 1]:
         # If the current element is greater than the next element, swap places
